ccbd9ceb25e030132714f50a4a988c6ecb067a15
ELBACHIR-08 <elbachir@digitalvirgo.com>
2026-07-08 22:24:44 +0000

    fix: generate env.js properly on vercel to prevent missing window.ENV

---
+++ api/env.js
@@ -0,0 +1,4 @@
+export default function handler(req, res) {
+  res.setHeader('Content-Type', 'application/javascript');
+  res.send(`window.ENV = { SUPABASE_URL: '${process.env.SUPABASE_URL}', SUPABASE_ANON_KEY: '${process.env.SUPABASE_ANON_KEY}' };`);
+}
